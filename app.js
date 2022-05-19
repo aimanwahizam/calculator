62e8a6180c5615d8381b1b509a6add3359d70d6c
aiman.wahizam <aiman.wahizam@nology.io>
2022-05-19 15:24:49 +0100

    made function to print number on one button only

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 // HTML elements
+//Try with one id first
+// const calculatorButton = document.querySelectorAll(".inputs__button")
+const testButton = document.getElementById("1");
 
-// Functions 
+console.log(testButton);
 
-// Element listeners
+// Functions
+
+// Try to console.log the id of the button
+
+const onClickCalculatorButton = () => {
+  console.log("1");
+};
+
+// Event listeners
+testButton.addEventListener("click", onClickCalculatorButton);
