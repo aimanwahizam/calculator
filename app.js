b2582d927b65d0359ce2dee6ba1f98b08d7e30d5
aiman.wahizam <aiman.wahizam@nology.io>
2022-05-19 15:57:49 +0100

    changed all h2 to buttons, id now in button

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 // HTML elements
 //Try with one id first
 // const calculatorButton = document.querySelectorAll(".inputs__button")
-const testButton = document.getElementById("1");
+const calculatorButton = document.querySelectorAll(".inputs__label");
 
+console.log(calculatorButton);
 
 // Functions
 // Try to console.log the id of the button
 
 const onClickCalculatorButton = () => {
-  console.log(testButton.value);
+  console.log(calculatorButton.value);
 };
 
 // Event listeners
-testButton.addEventListener ("click", onClickCalculatorButton);
+calculatorButton.forEach((button) =>
+  button.addEventListener("click", onClickCalculatorButton)
+);
