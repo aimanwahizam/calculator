63323b5d223dbdbab49bcec8d98821f97e8ebd74
aiman.wahizam <aiman.wahizam@nology.io>
2022-05-19 17:29:59 +0100

    trying to add ptags so can change charc color

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 // HTML elements---------------------------------------------------------------------------
 const calculatorButton = document.querySelectorAll(".inputs__button");
-const currentDisplay = document.querySelector(".display__current")
+const currentDisplay = document.querySelector(".display__current");
 
 // Empty array to store display
 const display = [];
+const symbols = ["/", "x", "-", "+"];
 
 // Functions --------------------------------------------------------------------------------
 const onClickCalculatorButton = (event) => {
   display.push(event.target.value);
-  currentDisplay.innerText += event.target.value;
+  console.log(display);
+
+  display.forEach((character) => {
+    if (symbols.includes(character)) {
+      currentDisplay.innerHTML = `<p class="diplay__color">${character}</p>`;
+    } else {
+      currentDisplay.innerHTML = `<p>${character}</p>`;
+    }
+  });
 };
 
 // Event listeners------------------------------------------------------------------------------
