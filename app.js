534056939c33395bd8c76cf3c09bf9bb212aa68d
aiman.wahizam <aiman.wahizam@nology.io>
2022-05-19 15:43:29 +0100

    buttons now con.log. had to change h2 to buttons

--- app.js
+++ app.js
@@ -3,15 +3,13 @@
 // const calculatorButton = document.querySelectorAll(".inputs__button")
 const testButton = document.getElementById("1");
 
-console.log(testButton);
 
 // Functions
-
 // Try to console.log the id of the button
 
 const onClickCalculatorButton = () => {
-  console.log("1");
+  console.log(testButton.value);
 };
 
 // Event listeners
-testButton.addEventListener("click", onClickCalculatorButton);
+testButton.addEventListener ("click", onClickCalculatorButton);
